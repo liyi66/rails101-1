daa8799ba0a57bd3a37d37b92c940f9e1155b4da
liyi <nxswliyi@163.com>
2017-01-05 15:13:01 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,13 +25,13 @@ end
     redirect_to groups_path
   end
 
-def update
-  @group = Group.find(params[:id])
+  def update
+    @group = Group.find(params[:id])
 
-  @group.update(group_params)
+    @group.update(group_params)
 
-  redirect_to groups_path, notice:"Update Success"
-end
+    redirect_to groups_path, notice: "Update Success"
+  end
 
 def destroy
   @group = Group.find(params[:id])
@@ -50,5 +50,4 @@ end
   def group_params
     params.require(:group).permit(:title, :description)
   end
-
 end
