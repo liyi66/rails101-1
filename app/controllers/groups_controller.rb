42d9b21ad442ddabae8a6910d2030d9520b961d9
liyi <nxswliyi@163.com>
2017-01-06 14:49:14 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,10 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
     @groups = Group.all
   end
 
-def show
-  @group = Group.find(params[:id])
-end
+  def show
+     @group = Group.find(params[:id])
+     @posts = @group.posts
+   end
 
 def edit
 
